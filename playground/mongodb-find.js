8ca4a1750cd505c56c862b699674414c2b3be703
eralpkor <eralp.ny@gmail.com>
2018-01-31 16:06:55 -0500

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,42 @@
+//const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    const db = client.db('TodoApp');
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5a72213d79b95e66d56c09a0')
+    //     }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+        
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+
+    db.collection('Users').find({
+        name: 'Eralp'
+    }).toArray().then((docs) => {
+        console.log('Users');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch');
+        
+    })
+     client.close();
+});
